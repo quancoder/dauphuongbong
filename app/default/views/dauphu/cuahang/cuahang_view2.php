6e07dd51e561f95f55a1eb7fcc71f749f93336a9
Le Quan <quanletrong@admicro.vn>
2019-04-03 01:22:12 +0700

    map test

--- app/default/views/dauphu/cuahang/cuahang_view2.php
+++ app/default/views/dauphu/cuahang/cuahang_view2.php
@@ -1,37 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Cambodia Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
     <style>
-        /* Set the size of the div element that contains the map */
         #map {
-            height: 400px;  /* The height is 400 pixels */
-            width: 100%;  /* The width is the width of the web page */
+            height: 70%;
+            width: 70%;
+        }
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
         }
     </style>
 </head>
 <body>
-<h3>My Google Maps Demo</h3>
-<!--The div element for the map -->
 <div id="map"></div>
 <script>
-    // Initialize and add the map
+    var map;
     function initMap() {
-        // The location of Uluru
-        var uluru = {lat: -25.344, lng: 131.036};
-        // The map, centered at Uluru
-        var map = new google.maps.Map(
-            document.getElementById('map'), {zoom: 4, center: uluru});
-        // The marker, positioned at Uluru
-        var marker = new google.maps.Marker({position: uluru, map: map});
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 21.0227788, lng: 105.8019441},
+            zoom: 12
+        });
+
+        //call marker function
+        addMarker({lat:21.0127788,lng:105.799939});
+        addMarker({lat:21.0227788,lng:105.799939});
+        addMarker({lat:21.0327788,lng:105.799939});
+        addMarker({lat:21.0427788,lng:105.799939});
+
+        addMarker({lat:21.0127788,lng:105.719939});
+        addMarker({lat:21.0227788,lng:105.729939});
+        addMarker({lat:21.0327788,lng:105.739939});
+        addMarker({lat:21.0427788,lng:105.749939});
+
+        addMarker({lat:21.0117788,lng:105.700939});
+        addMarker({lat:21.0227788,lng:105.710939});
+        addMarker({lat:21.0337788,lng:105.720939});
+        addMarker({lat:21.0447788,lng:105.730939});
+
+        //add marker function
+        function addMarker(coords){
+            var marker = new google.maps.Marker({
+                position:coords,
+                map:map,
+                icon:'images/shoppingcart_pinlet-2-medium.png',
+            });
+        }
     }
 </script>
-<!--Load the API from the specified URL
-* The async attribute allows the browser to render the page while the API loads
-* The key parameter will contain your own API key (which is not needed for this tutorial)
-* The callback parameter executes the initMap() function
--->
-<script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2ynq4k54qX5memyZUqy8wZ9ZUDxe4jRk&callback=initMap">
-</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDg945v29-ecA_WktGp3cXxK8e6Y34if3Y
+        &callback=initMap" async defer></script>
 </body>
 </html>
