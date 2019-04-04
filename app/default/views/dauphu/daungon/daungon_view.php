8c6f29c043999a25b672c952583a6e51048cafba
hoangnd <hoangnd@admin>
2019-04-04 11:24:02 +0700

    fix

--- app/default/views/dauphu/daungon/daungon_view.php
+++ app/default/views/dauphu/daungon/daungon_view.php
@@ -41,3 +41,282 @@
         </div>
     </div>
 </div>
+<style>
+    dl, ol, ul {
+        margin-bottom: 0 !important;
+    }
+    .collection_head {
+        padding: 20px 0px;
+        background: #fff;
+        margin-bottom: 20px;
+    }
+    .page_head {
+        position: relative;
+    }
+    .page_head .collection_title {
+        font-size: 28px;
+        color: #555555;
+        padding-bottom: 0;
+        margin-bottom: 0;
+        text-align: center;
+        text-transform: uppercase;
+    }
+    .page_head p.countPd {
+        color: #555555;
+        font-size: 13px;
+        text-align: center
+    }
+    
+    .collection_head .display-product-option {
+        position: absolute;
+        top: 50%;
+        right: 0px;
+        transform: translateY(-50%);
+        -webkit-transform: translateY(-50%);
+        -ms-transform: translateY(-50%);
+        -o-transform: translateY(-50%);
+    }
+ 
+.browse-tags p {
+    display: inline-block;
+    line-height: 30px;
+    text-align: center
+}
+.browse-tags select {
+    border: 1px solid #ccc;
+    outline: none;
+}
+.bg-white{background-color: white}
+.post-module {
+    position: relative;
+    z-index: 1;
+    display: block;
+    background: #FFFFFF;
+    min-width: 100%;
+    height: 370px;
+    margin-bottom: 20px;
+    -webkit-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+    -moz-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+    box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+
+    -webkit-transition: height 0.5s ease-out, display 0.5s ease-out; 
+}
+.post-module .thumbnail {
+    background: #000000;
+    height: 330px;
+    overflow: hidden;
+}
+.post-module .thumbnail img {
+    display: block;
+    width: 120%;
+    -webkit-transition: all 0.3s linear 0s;
+    -moz-transition: all 0.3s linear 0s;
+    -ms-transition: all 0.3s linear 0s;
+    -o-transition: all 0.3s linear 0s;
+    transition: all 0.3s linear 0s;
+}
+.post-module .post-content {
+    position: absolute;
+    bottom: 0;
+    background: #FFFFFF;
+    width: 100%;
+    padding: 20px 15px 15px;
+    -webkti-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    -moz-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    -ms-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    -o-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+}
+.post-module .post-content .title {
+    margin: 0;
+    padding: 0 0 10px;
+    color: #333333;
+    font-size: 20px;
+    font-weight: 700;
+    text-align: center;
+}
+.post-module .post-content .sub_title {
+    margin: 0;
+    padding: 0 0 10px;
+    color: #e74c3c;
+    font-size: 17px;
+    font-weight: 400;
+    text-align: center;
+    
+}
+
+.post-module .post-content .description {
+    display: none;
+    color: #666666;
+    margin-bottom: 10px;
+    font-size: 14px;
+    -webkit-transition: opacity .5s ease-out; 
+}
+
+.btn-cart{
+    text-align: center;
+    margin-bottom: 0;
+}
+</style>
+<div id="SECTION23" style="height: auto" class="widget-section ladi-drop ui-droppable collection_head bg-white" lp-type="widget_section" lp-widget="widget" lp-lang="SECTION" lp-display="block">
+    <div class="container-fluid">
+        <div class="boxSortHead mb-4">
+            <div class="page_head">
+                <h1 class="collection_title ins_title">
+                    Sản phẩm
+                </h1>
+                <p class="countPd">(0 sản phẩm)</p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="post-module">
+                    <div class="thumbnail">
+                        <img src="http://media.doisongphapluat.com/448/2016/3/23/dau-phu-sot-cay1.png">
+                    </div>
+                    <div class="post-content">
+                        
+                        <h1 class="title">Đậu phụ non sốt cay</h1>
+                        <p class="description" style="display: none; height: 100px; opacity: 1;">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                        <p class="btn-cart">
+                            <button type="button" class="btn btn-sm btn-success">Xem thêm</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+        </div>
+    </div> 
+
+</div>
+<script>
+    $(function(){
+        $('.post-module').hover(function() {
+            $(this).find('.description').stop().animate({
+              height: "toggle",
+              opacity: "toggle"
+            }, 200);
+          });
+    });
+
+</script>
+
+
